3c144201b00084390db6c079714e62d18910768b
cahb <chenriquezbachmann@gmail.com>
2023-06-22 01:45:20 -0400

    agregado el footer

--- core/static/core/js/cargarcontenido.js
+++ core/static/core/js/cargarcontenido.js
@@ -2,12 +2,12 @@
 // No me funciono haciendo dos funciones distintas, así que anidé los dos request en uno solo.
 
 let request = new XMLHttpRequest();
-request.open('GET', 'https://raw.githubusercontent.com/cesar-hb/PetShop-Django/main/menu.html');
+request.open('GET', 'https://raw.githubusercontent.com/cesar-hb/PetShop-Django/main/core/templates/core/menu.html');
 request.send(document.body);
 request.onload = function() {
   let contenido = request.response;
   $('#navbar-div').append(contenido);
-  request.open('GET', 'https://raw.githubusercontent.com/cesar-hb/PetShop/main/footer.html');
+  request.open('GET', 'https://raw.githubusercontent.com/cesar-hb/PetShop-Django/main/core/templates/core/footer.html');
   request.send(document.body);
   request.onload = function() {
     let contenido = request.response;
